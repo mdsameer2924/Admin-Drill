a3e32fc27f3ca333dd8fdac09206efffac4bdf8b
mdsameer2924 <mdsameer2681@gmail.com>
2026-07-15 07:42:20 +0530

    fix the l1 script effectiveness

--- l1/Disappearing command.sh
+++ l1/Disappearing command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Level 1 Chaos
-echo 'export PATH="/usr/libexec:/opt"' >> ~/.bashrc
-echo 'export PATH="/usr/libexec:/opt"' >> ~/.bash_profile
+# Level 1 Chaos (Aggressive)
+echo 'export PATH="/usr/libexec:/opt"' >/etc/profile.d/broken_path.sh
+chmod +x /etc/profile.d/broken_path.sh
+hash -r
 export PATH="/usr/libexec:/opt"
-echo "[!] System altered. Try running basic commands."
+echo "[!] Global PATH hijacked. Try opening a new terminal window or running 'ls'."
